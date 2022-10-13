264dceff96f4d6e521fdd214c8dd83ecf9b49dca
Antoine <florentantoine32@gmail.com>
2022-10-14 01:47:23 +0200

    navbar advancement

---
+++ main.js
@@ -0,0 +1,9 @@
+function Hamburger(){
+    var nav = document.querySelector("nav");
+    nav.style.right = 0;
+}
+
+function Close(){
+    var nav = document.querySelector("nav");
+    nav.style.right = "-80%";
+}
