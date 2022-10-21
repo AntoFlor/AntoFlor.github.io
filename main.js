d3292450b22e7e9586cd4468eea79da9fdf50ba4
Antoine <florentantoine32@gmail.com>
2022-10-21 23:37:30 +0200

    navbar advancement + bg img

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+window.onload = navMenuDisplayNone;
+
+function navMenuDisplayNone()
+{
+    document.querySelector("#projets").style.display = "none"
+}
+
 function Hamburger(){
     var nav = document.querySelector("nav");
     nav.style.right = 0;
@@ -6,4 +13,19 @@ function Hamburger(){
 function Close(){
     var nav = document.querySelector("nav");
     nav.style.right = "-80%";
+}
+
+function Projects(){
+    if (document.querySelector("#projets").style.display == "none")
+    {
+        document.querySelector("#projets").style.display = "flex";
+        document.querySelector("#arrow1").style.transform = "rotate(90deg)";
+        console.log("flex");
+    }
+    else if (document.querySelector("#projets").style.display != "none")
+    {
+        document.querySelector("#projets").style.display = "none";
+        document.querySelector("#arrow1").style.transform = "rotate(0deg)";
+        console.log("none");
+    }
 }
